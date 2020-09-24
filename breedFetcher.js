9aefbc553e648a7db90d0ba2157e1cd52ebf0323
Etienne LC <etlecoent@gmail.com>
2020-09-24 19:39:37 +0000

    Did some refactoring and unit testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,42 +1,21 @@
-const request = require('request');
 
-const userInputs = function(callback) {
-  const readline = require('readline');
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  let questions = {
-    breed : 'What is the breed? ',
-  };
-  
-  let requestObj = {};
-  
-  
-  rl.question(questions.breed, (answer) => {
-    requestObj["breed"] = answer;
-    callback((requestObj['breed']));
-    rl.close();
-  });
-};
-
-
-const breedFetcher = (breedString) => {
-  
+const fetchBreedDescription = function(breedString, callback) {
+  const request = require('request');
   const url = "https://api.thecatapi.com/v1/breeds/search?q=";
   
   request.get(url + breedString, (err, response, body) => {
     if (err) {
-      throw err;
+      callback(err, null);
     }
     const data = JSON.parse(body);
+    // console.log(data);
     if (!data.length) {
-      console.log("Breed not found!");
+      callback(null, "This breed does not exist");
+      // callback(err, null);
     } else {
-      console.log(data[0]['description']);
+      callback(null, data[0]['description'].trim());
     }
   });
 };
 
-userInputs(breedFetcher);
+module.exports = { fetchBreedDescription };
---
+++ userInputs.js
@@ -0,0 +1,24 @@
+// OLD DO NOT TOUCH !
+
+// const userInputs = function() {
+//   const readline = require('readline');
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+  
+//   let questions = {
+//     breed : 'What is the breed? ',
+//   };
+  
+//   let requestObj = {};
+  
+  
+//   rl.question(questions.breed, (answer) => {
+//     requestObj["breed"] = answer;
+//     breedFetcherDescription((requestObj['breed']));
+//     rl.close();
+//   });
+// };
+
+// module.exports = { userInputs };
